b5ed06616a86a8b04d54e102397c3a3389dc1ba7
tamami <tamami.oka@gmail.com>
2018-01-18 16:07:49 +0700

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientXsrfModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HttpXsrfInterceptor } from '@angular/common/http/src/xsrf';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    HttpClientModule,
+    HttpClientXsrfModule
   ],
-  providers: [],
+  providers: [  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
